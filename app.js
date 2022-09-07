e6585006d411f9b64b208c7b5dd871063e0a759e
10high <99827666+10high@users.noreply.github.com>
2022-09-07 19:18:05 +0200

    new input system: input/beforeinput/pointerdown

--- app.js
+++ app.js
@@ -1,187 +1,128 @@
-const isDigitKey = key => {
-    const digitKeys = {
-        0: "#key0",
-        1: "#key1",
-        2: "#key2",
-        3: "#key3",
-        4: "#key4",
-        5: "#key5",
-        6: "#key6",
-        7: "#key7",
-        8: "#key8",
-        9: "#key9"
+const inputData = {
+    existingInput: "",
+    isDigitKey: key => {
+        const digitKeys = {
+            0: "#key0",
+            1: "#key1",
+            2: "#key2",
+            3: "#key3",
+            4: "#key4",
+            5: "#key5",
+            6: "#key6",
+            7: "#key7",
+            8: "#key8",
+            9: "#key9"
+        }
+        return Object.hasOwn(digitKeys, key) ? digitKeys[key] : false;
+    },
+    inputFieldIDs: ["remInput", "minScreenInput", "maxScreenInput", "minClampInput", "maxClampInput"],
+
+    isValidInputField: (inputElement = document.activeElement) => inputData.inputFieldIDs.includes(inputElement.id),
+
+    isMinLength: (inputElement = document.activeElement) => {
+        inputElement.classList.remove("calculator__inputField--invalid");
+        if (inputElement.value.length > 0) {
+            return true;
+        } else {
+            inputElement.classList.add("calculator__inputField--invalid");
+            return false;
+        }
     }
-    return Object.hasOwn(digitKeys, key) ? digitKeys[key] : false;
 }
 
-inputFieldIDs = ["remInput", "minScreenInput", "maxScreenInput", "minClampInput", "maxClampInput"];
-
-const isValidInputField = (inputElement = document.activeElement) => inputFieldIDs.includes(inputElement.id);
-
-const specialKeys = ["ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown", "Backspace", "Delete", "Tab"];
-
-const isMinLength = (inputElement = document.activeElement) => {
-    inputElement.classList.remove("calculator__inputField--invalid");
-    if (inputElement.value.length > 0) {
-        return true;
-    } else {
-        inputElement.classList.add("calculator__inputField--invalid");
-        return false;
-    }
+const toggleKeypadPressed = element => {
+    element.classList.add("keypad__button--pressed")
+    setTimeout(() => {
+        element.classList.remove("keypad__button--pressed");
+    }, 300);
 }
-
-const insertNewValue = (inputtedValue, inputElement = document.activeElement) => {
-    let existingValue = inputElement.value;
-    if (existingValue === "0") {
-        existingValue = "";
-    }
-    let selectionStart = inputElement.selectionStart;
-    let selectionEnd = inputElement.selectionEnd;
-    inputElement.value = inputElement.value.substring(0, selectionStart)
-        + inputtedValue
-        + inputElement.value.substring(selectionEnd, inputElement.value.length);
-}
-
-const isNotMaxLength = (inputElement = document.activeElement) => {
-    return inputElement.value.length < 4;
-}
-
-const toggleKeypadPressed = element => element.classList.toggle("keypad__button--pressed");
 
 const copyToClipboard = () => navigator.clipboard.writeText(document.querySelector(".calculator__output").value);
 
+const isKeyEnter = (event) => {
+    if (event.key === "Enter") {
+        calculateClamp();
+    }
+}
 
-
-const keyClickedDown = (event) => {
+const digitKeyClicked = (event) => {
     event.preventDefault();
     toggleKeypadPressed(event.target);
-    if (isDigitKey(event.target.value) && isValidInputField() && isNotMaxLength()) {
-        document.activeElement.classList.remove("calculator__inputField--invalid");
-        insertNewValue(event.target.value);
+    if (!inputData.isValidInputField()) {
+        console.log("not valid input field");
         return;
     }
-    if (isDigitKey(event.target.value) && isValidInputField() && !isNotMaxLength()) {
-        let selectionLength = document.activeElement.selectionEnd - document.activeElement.selectionStart;
-        let inputFieldLength = document.activeElement.value.length;
-        if ((inputFieldLength - selectionLength) + 1 < 5) {
-            document.activeElement.classList.remove("calculator__inputField--invalid");
-            insertNewValue(event.target.value);
-            return;
+    const activeInputField = document.activeElement;
+    const existingInput = activeInputField.value;
+    const clickValue = event.target.value;
+    console.log(`beforeInput: ${existingInput}`);
+    console.log(`event target value on click: ${clickValue}`);
+    if (!inputData.isDigitKey(clickValue)) {
+        console.log(`Click value is not digit key: ${clickValue}`);
+        if (clickValue === "=") {
+            console.log("Click Vlaue is Equals");
+            calculateClamp();
         }
+        return;
     }
-    if (event.target.value === "=") {
-        calculateClamp();
+    let selectionStart = activeInputField.selectionStart;
+    let selectionEnd = activeInputField.selectionEnd;
+    const selectionLength = selectionEnd - selectionStart;
+    if ((existingInput.length - selectionLength) + 1 < 5) {
+        activeInputField.classList.remove("calculator__inputField--invalid");
+        activeInputField.value = activeInputField.value.substring(0, selectionStart)
+            + clickValue
+            + activeInputField.value.substring(selectionEnd, activeInputField.value.length);
         return;
     }
 }
 
-const keyClickedUp = (event) => toggleKeypadPressed(event.target);
+const captureKeyboardInput = (event) => {
+    existingInput = event.target.value;
+    inputData.existingInput = existingInput;
+    console.log(`beforeInput: ${inputData.existingInput}`);
+}
 
-const keyPressedDown = (event) => {
-    if (isDigitKey(event.key) && isValidInputField() && isNotMaxLength()) {
-        let key = document.querySelector(isDigitKey(event.key));
-        toggleKeypadPressed(key);
-        document.activeElement.classList.remove("calculator__inputField--invalid");
-        return;
-    }
-    if (isDigitKey(event.key) && isValidInputField() && !isNotMaxLength()) {
-        let selectionLength = document.activeElement.selectionEnd - document.activeElement.selectionStart;
-        let inputFieldLength = document.activeElement.value.length;
-        if ((inputFieldLength - selectionLength) + 1 < 5) {
-            let key = document.querySelector(isDigitKey(event.key));
-            toggleKeypadPressed(key);
-            document.activeElement.classList.remove("calculator__inputField--invalid");
+const monitorKeyboardInput = (event) => {
+    const newInput = event.data;
+    const insertedText = Array.from(event.target.value);
+    if (newInput === null) {
+        console.log(`newINput is null. ID; ${newInput}`);
+        if (insertedText.length > 4) {
+            console.log(`insertedText length: ${insertedText.length}`);
+            event.target.value = inputData.existingInput;
             return;
         }
-    }
-    if (event.ctrlKey && event.key === "c" || event.ctrlKey && event.key === "a") {
-        return;
-    }
-    if (event.ctrlKey && event.key === "r") {
-        return;
-    }
-    if (event.ctrlKey && event.key === "v") {
-        if (isValidInputField()) {
-            let existingFieldValue = document.activeElement.value;
-            setTimeout(() => {
-                let clipboardContent = Array.from(document.activeElement.value);
-                for (item of clipboardContent) {
-                    if (!isDigitKey(item)) {
-                        document.activeElement.value = existingFieldValue;
-                        return;
-                    }
-                }
-                if (clipboardContent.length > 4) {
-                    document.activeElement.value = existingFieldValue;
-                    return;
-                }
-            }, 100);
+        for (item of insertedText) {
+            if (!inputData.isDigitKey(item)) {
+                event.target.value = inputData.existingInput;
+                console.log(`Inserted, replace value with: ${inputData.existingInput}`);
+                return;
+            }
         }
         return;
     }
-    if (isValidInputField() && !isDigitKey(event.key) && !specialKeys.includes(event.key)) {
-        event.preventDefault();
-    }
-    if (isValidInputField() && !isNotMaxLength() && !specialKeys.includes(event.key)) {
-        event.preventDefault();
-        if (isDigitKey(event.key)) {
-            let key = document.querySelector(isDigitKey(event.key));
-            toggleKeypadPressed(key);
-            return;
-        }
-    }
-    if (event.key === "c") {
-        let key = document.querySelector("#keyC");
+    if (newInput === "c") {
+        const key = document.getElementById("keyC");
         toggleKeypadPressed(key);
         key.click();
         return;
     }
-    if (event.key === "Enter" && document.activeElement.id === "clipboard") {
-        toggleKeypadPressed(document.activeElement);
-        copyToClipboard();
-        return;
+    if (insertedText.length > 4) {
+        event.target.value = inputData.existingInput;
     }
-
-    if (event.key === "=" || event.key === "Enter") {
-        toggleKeypadPressed(document.querySelector("#keyEquals"));
-        calculateClamp();
-        return;
-    }
-}
-
-const keyPressedUp = (event) => {
-    if (isDigitKey(event.key)) {
-        let key = document.querySelector(isDigitKey(event.key));
-        toggleKeypadPressed(key);
-        return
-    }
-    if (event.ctrlKey && event.key === "c") {
-        return;
-    }
-    if (event.key === "c") {
-        toggleKeypadPressed(document.querySelector("#keyC"));
-        return;
-    }
-    if (event.key === "Enter" && document.activeElement.id === "clipboard") {
-        toggleKeypadPressed(document.activeElement);
-        return;
-    }
-    if (event.key === "Enter" || event.key === "=") {
-        toggleKeypadPressed(document.querySelector("#keyEquals"));
-        return;
-    }
-}
-
-const inputChanged = (event) => {
-    if (!isDigitKey(event.data)) {
-        if (event.inputType.includes("delete")) {
+    for (item of insertedText) {
+        if (!inputData.isDigitKey(item)) {
+            event.target.value = inputData.existingInput;
+            console.log(`Inserted, replace value with: ${inputData.existingInput}`);
             return;
         }
-        event.target.setAttribute("readonly", "readonly");
-        setTimeout(() => {
-            event.target.removeAttribute("readonly");
-        }, 300)
     }
+    console.log(`Successfully inserted: ${newInput}`);
+    const key = document.querySelector(inputData.isDigitKey(newInput));
+    console.log(`key element: ${key}`);
+    toggleKeypadPressed(key);
+    event.target.classList.remove("calculator__inputField--invalid");
 }
 
 const elementArray = query => document.querySelectorAll(query);
@@ -199,8 +140,8 @@ const calculateClamp = () => {
         "maxClampInput": 0
     }
     let invalidFieldInputs = 0;
-    for (inputField of inputFieldIDs) {
-        if (!isMinLength(document.querySelector(`#${inputField}`))) {
+    for (inputField of inputData.inputFieldIDs) {
+        if (!inputData.isMinLength(document.querySelector(`#${inputField}`))) {
             invalidFieldInputs++;
         } else {
             inputFieldValues[inputField] = parseInt(document.querySelector(`#${inputField}`).value);
@@ -219,10 +160,9 @@ const calculateClamp = () => {
     }
 }
 
-addEventListener(elementArray(".keypad__button"), "mousedown", keyClickedDown);
-addEventListener(elementArray(".keypad__button"), "mouseup", keyClickedUp);
-//document.addEventListener("keydown", keyPressedDown);
-//document.addEventListener("keyup", keyPressedUp);
-document.querySelector(".calculator__clipboard").addEventListener("click", copyToClipboard);
-addEventListener(elementArray(".calculator__inputField"), "beforeinput", inputChanged);
+addEventListener(elementArray(".keypad__button"), "pointerdown", digitKeyClicked);
+document.querySelector(".calculator__clipboard").addEventListener("pointerdown", copyToClipboard);
+addEventListener(elementArray(".calculator__inputField"), "beforeinput", captureKeyboardInput);
+addEventListener(elementArray(".calculator__inputField"), "input", monitorKeyboardInput);
+document.addEventListener("keydown", isKeyEnter);
 
